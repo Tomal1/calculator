dec8affe74e1eae89b191c2c99ef4bdf8be2d769
MI <minhazulislam@Minhazuls-MacBook-Air.local>
2023-08-04 01:46:28 +0100

    layout changed and css removed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,42 @@
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="calculator-grid">
-      <div className="output"><div/>
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
-      </div>
-      <button className='span-two'>AC</button>
-      <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className='span-two'>=</button>
+const App = () => {
 
-    </div>
-  );
+  return(
+    <div className="container">
+      <div className="calculator">
+        <form action="">
+          <div>
+            <input type="text"/>
+          </div>
+          <div>
+            <input type="button" value="AC"/>
+            <input type="button" value="DE"/>
+            <input type="button" value="."/>
+            <input type="button" value="/"/>
+          </div>
+          <div>
+            <input type="button" value="7"/>
+            <input type="button" value="8"/>
+            <input type="button" value="9"/>
+            <input type="button" value="*"/>
+          </div>
+          <div>
+            <input type="button" value="4"/>
+            <input type="button" value="5"/>
+            <input type="button" value="6"/>
+            <input type="button" value="+"/>
+          </div>
+          <div>
+            <input type="button" value="3"/>
+            <input type="button" value="2"/>
+            <input type="button" value="1"/>
+            <input type="button" value="-"/>
+          </div>
+       </form> 
+      </div>
+    </div>)
 }
 
 export default App;
